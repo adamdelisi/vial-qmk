31c8649db801120ad8e886028b78e31c18077d21
adamdelisi <51133980+adamdelisi@users.noreply.github.com>
2022-03-08 11:17:57 -0500

    update keymap
    update keymap

--- keyboards/handwired/void_ergo/keymaps/default/keymap.c
+++ keyboards/handwired/void_ergo/keymaps/default/keymap.c
@@ -20,6 +20,7 @@ enum layer_names {
   _QWERTY,
   _RAISE,
   _LOWER,
+  _MEDIA,
   _ADJUST
 
 };
@@ -81,15 +82,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                       |------------------------|  |--------------------------|
  */
 [_QWERTY] = LAYOUT_void_ergo(
-    KC_ESC,     KC_Q,     KC_W,      KC_E,       KC_R,     KC_T,                     KC_Y,       KC_U,     KC_I,       KC_O,     KC_P,     KC_BSPC,
-    KC_TAB,    KC_A,     KC_S,      KC_D,       KC_F,     KC_G,                     KC_H,       KC_J,     KC_K,       KC_L,     KC_SCLN,  KC_ENT,
-    KC_LSFT,    KC_Z,     KC_X,      KC_C,        KC_V,      KC_B,                   KC_N,       KC_M,     KC_COMM,    KC_DOT,   KC_SLSH,  KC_RSFT,
-                KC_MPLY,  KC_LALT,  MO(_LOWER), KC_LCTL,   KC_SPC,                  KC_LGUI,   KC_SPC,   MO(_RAISE), KC_RGUI,  MO(_ADJUST)
+    KC_ESC,     KC_Q,     KC_W,      KC_E,       KC_R,     KC_T,                    KC_Y,       KC_U,     KC_I,       KC_O,     KC_P,       KC_BSPC,
+    KC_TAB,    KC_A,     KC_S,      KC_D,       KC_F,     KC_G,                     KC_H,       KC_J,     KC_K,       KC_L,     KC_SCLN,    KC_ENT,
+    KC_LSFT,    KC_Z,     KC_X,      KC_C,        KC_V,      KC_B,                  KC_N,       KC_M,     KC_COMM,    KC_DOT,   KC_SLSH,    KC_QUOTE,
+                KC_MPLY,  KC_LALT,  MO(_LOWER), KC_LCTL,   KC_SPC,                  KC_LGUI,   KC_SPC,   MO(_RAISE),  MO(_ADJUST),  MO(_MEDIA)
 ),
 
 [_LOWER] = LAYOUT_void_ergo(
     KC_ESC,     KC_1,     KC_2,     KC_3,       KC_4,     KC_5,                     KC_6,       KC_7,     KC_8,       KC_9,     KC_0,     KC_BSPC,
-    KC_TRNS,    KC_LEFT,  KC_DOWN,  KC_UP,      KC_RIGHT, KC_TRNS,                  KC_LEFT,    KC_DOWN,     KC_UP,      KC_RIGHT, KC_TRNS,  KC_TRNS,
+    KC_TRNS,    KC_LEFT,  KC_DOWN,  KC_UP,      KC_RIGHT, KC_TRNS,                  KC_TRNS,    KC_LEFT,    KC_DOWN,     KC_UP,    KC_RIGHT,  KC_TRNS,
     KC_TRNS,    BRINGBB,  BRINGB,   BRINGF,     BRINGFF,  KC_TRNS,                  KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
                 KC_TRNS,  SCRNSHT,  MO(_LOWER), KC_SPC,   SCRNREC,                  KC_TRNS,    KC_ENT,      MO(_RAISE), KC_RALT,  KC_TRNS
 ),
@@ -101,10 +102,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_TRNS,  KC_LGUI,  MO(_LOWER), KC_SPC,   KC_TRNS,                  KC_TRNS,    KC_ENT,   MO(_RAISE), KC_RALT,  KC_TRNS
 ),
 
-[_ADJUST] = LAYOUT_void_ergo(
+[_MEDIA] = LAYOUT_void_ergo(
     KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_MPLY,
     KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_MNXT,
     KC_F1,      KC_F2,    KC_F3,    KC_F4,      KC_F5,    KC_F6,                    KC_F7,      KC_F8,    KC_F9,      KC_F10,   KC_F11,   KC_MPRV,
+                KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  RESET,                    RESET,      KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS
+),
+
+[_ADJUST] = LAYOUT_void_ergo(
+    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_F1,      KC_F2,    KC_F3,    KC_F4,      KC_F5,    KC_F6,                    KC_F7,      KC_F8,    KC_F9,      KC_F10,   KC_F11,   KC_TRNS,
                 KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  RESET,                    RESET,      KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS
 )
 };

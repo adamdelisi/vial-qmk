d2a13d9a0cf80f180ae7ba5625f0ced9673c112b
adamdelisi <51133980+adamdelisi@users.noreply.github.com>
2022-03-07 22:37:22 -0500

    Revert "big ol changes. a whole lotta them"
    This reverts commit a9b8db740c9ad3cdc5ff06c236f9d954a312e9a2.

--- keyboards/handwired/void_ergo/keymaps/victorlucachi/keymap.c
+++ keyboards/handwired/void_ergo/keymaps/victorlucachi/keymap.c
@@ -34,35 +34,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
  * |KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT |
  * |--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
- *                         KC_NO, KC_NO, KC_LGUI,   LOWER,  KC_SPC|  |  KC_ENT,  RAISE,  KC_RALT, KC_NO, KC_NO
+ *                         KC_NO, KC_NO, KC_LGUI,   LOWER,  KC_SPC|  |  KC_ENT,  RAISE,  KC_RALT, KC_NO, KC_NO 
  *                                       |------------------------|  |--------------------------|
  */
 [_QWERTY] = LAYOUT_void_ergo(
-    KC_ESC,     KC_Q,     KC_W,     KC_E,       KC_R,     KC_T,                     KC_Y,       KC_U,     KC_I,       KC_O,     KC_P,     KC_BSPC,
-    KC_TAB,    KC_A,     KC_S,     KC_D,       KC_F,     KC_G,                     KC_H,       KC_J,     KC_K,       KC_L,     KC_SCLN,  KC_ENT,
-    KC_LSFT,    KC_Z,     KC_X,    KC_C,       KC_V,     KC_B,                     KC_N,       KC_M,     KC_COMM,    KC_DOT,   KC_SLSH,  KC_RSFT,
-                KC_MPLY,  KC_LALT,  MO(_LOWER), KC_LCTL,   KC_SPC,                  KC_LGUI,   KC_SPC,   MO(_RAISE), KC_RGUI,  KC_MUTE
+    KC_TAB,     KC_Q,     KC_W,     KC_E,       KC_R,     KC_T,                     KC_Y,       KC_U,     KC_I,       KC_O,     KC_P,     KC_BSPC,
+    KC_LSFT,    KC_A,     KC_S,     KC_D,       KC_F,     KC_G,                     KC_H,       KC_J,     KC_K,       KC_L,     KC_SCLN,  KC_QUOT,
+    KC_LCTL,    KC_Z,     KC_X,     KC_C,       KC_V,     KC_B,                     KC_N,       KC_M,     KC_COMM,    KC_DOT,   KC_SLSH,  KC_RSFT,
+                KC_MPLY,  KC_LALT,  MO(_LOWER), KC_SPC,   KC_TRNS,                  KC_TRNS,    KC_ENT,   MO(_RAISE), KC_RGUI,  KC_MUTE
 ),
 
 [_LOWER] = LAYOUT_void_ergo(
-    KC_ESC,     KC_1,     KC_2,     KC_3,       KC_4,     KC_5,                     KC_6,       KC_7,     KC_8,       KC_9,     KC_0,     KC_BSPC,
+    KC_ESC,     KC_EXLM,  KC_AT,    KC_HASH,    KC_DLR,   KC_PERC,                  KC_CIRC,    KC_AMPR,     KC_ASTR,    KC_LPRN,  KC_RPRN,  KC_BSPC,
     KC_TRNS,    KC_LEFT,  KC_DOWN,  KC_UP,      KC_RIGHT, KC_TRNS,                  KC_LEFT,    KC_DOWN,     KC_UP,      KC_RIGHT, KC_TRNS,  KC_TRNS,
     KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
                 KC_TRNS,  KC_LGUI,  MO(_LOWER), KC_SPC,   KC_TRNS,                  KC_TRNS,    KC_ENT,      MO(_RAISE), KC_RALT,  KC_TRNS
 ),
 
 [_RAISE] = LAYOUT_void_ergo(
-    KC_ESC,     KC_EXLM,  KC_AT,    KC_HASH,    KC_DLR,   KC_PERC,                  KC_CIRC,    KC_AMPR,     KC_ASTR,    KC_LPRN,  KC_RPRN,  KC_BSPC,
+    KC_ESC,     KC_1,     KC_2,     KC_3,       KC_4,     KC_5,                     KC_6,       KC_7,     KC_8,       KC_9,     KC_0,     KC_BSPC,
     KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_MINS,    KC_EQL,   KC_LCBR,    KC_RCBR,  KC_PIPE,  KC_GRV,
     KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_UNDS,    KC_PLUS,  KC_LBRC,    KC_RBRC,  KC_BSLS,  KC_TILD,
                 KC_TRNS,  KC_LGUI,  MO(_LOWER), KC_SPC,   KC_TRNS,                  KC_TRNS,    KC_ENT,   MO(_RAISE), KC_RALT,  KC_TRNS
 ),
 
 [_ADJUST] = LAYOUT_void_ergo(
-    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_F1,      KC_F2,    KC_F3,    KC_F4,      KC_F5,    KC_F6,                    KC_F7,      KC_F8,    KC_F9,      KC_F10,   KC_F11,   KC_F12,
-                KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  RESET,                    RESET,      KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS
+    KC_TRNS,    RGUI(KC_1),   RGUI(KC_2),   RGUI(KC_3),   RGUI(KC_4),   RGUI(KC_5),               RGUI(KC_6), RGUI(KC_7), RGUI(KC_8), RGUI(KC_9), RGUI(KC_0), KC_TRNS,
+    KC_TRNS,    RGUI(KC_INS), RGUI(KC_PGDN),RGUI(KC_HOME),RGUI(KC_PGUP),KC_TRNS,                  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+    KC_F1,      KC_F2,        KC_F3,        KC_F4,        KC_F5,        KC_F6,                    KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
+                KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,      RESET,                    RESET,      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS
 )
 };
 
@@ -74,25 +74,76 @@ layer_state_t layer_state_set_user(layer_state_t state) {
  * ROTARY ENCODER
  */
 
-bool encoder_update_kb(uint8_t index, bool clockwise) {
-    return encoder_update_user(index, clockwise);
+static bool tabbing = false;
+static uint16_t tabtimer;
+#define TABBING_TIMER 750
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+  if (index == 0) {
+    if (IS_LAYER_ON(_RAISE)) {
+      if (clockwise) {
+        tabtimer = timer_read();
+        if(!tabbing) {
+          register_code(KC_LALT);
+          tabbing = true;
+        }
+        tap_code(KC_TAB);
+      } else {
+        tabtimer = timer_read();
+        if(!tabbing) {
+          register_code(KC_LALT);
+          tabbing = true;
+        }
+
+        register_code(KC_LSFT);
+        tap_code(KC_TAB);
+        unregister_code(KC_LSFT);
+      }
+    } else {
+      if (clockwise) {
+        tap_code(KC_MPRV);
+      } else {
+        tap_code(KC_MNXT);
+      }
+    }
+  }
+  else if (index == 1) {
+    if (IS_LAYER_ON(_RAISE)) {
+      if (clockwise) {
+        tabtimer = timer_read();
+        if(!tabbing) {
+          register_code(KC_LALT);
+          tabbing = true;
+        }
+        tap_code(KC_TAB);
+      } else {
+        tabtimer = timer_read();
+        if(!tabbing) {
+          register_code(KC_LALT);
+          tabbing = true;
+        }
+
+        register_code(KC_LSFT);
+        tap_code(KC_TAB);
+        unregister_code(KC_LSFT);
+      }
+    } else {
+      if (clockwise) {
+        tap_code(KC_VOLU);
+      } else {
+        tap_code(KC_VOLD);
+      }
+    }
+  } 
 }
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
-        if (clockwise) {
-            tap_code(KC_MNXT);
-        } else {
-            tap_code(KC_MPRV);
-        }
-    } else if (index == 1) { /* Second encoder */
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
+void matrix_scan_user(void) {
+  if(tabbing) {
+    if (timer_elapsed(tabtimer) > TABBING_TIMER) {
+      unregister_code(KC_LALT);
+      tabbing = false;
     }
-    return true;
+  }
 }
 
 void matrix_init_user(void) {

d7aea552a2406cb12130b737426d444b33fcb509
adamdelisi <51133980+adamdelisi@users.noreply.github.com>
2022-05-14 12:26:18 -0400

    fix plaid

---
+++ keyboards/dm9records/plaid/keymaps/vial/config.h
@@ -0,0 +1,16 @@
+// These VID/PID is not verified. Use at your own risk.
+// #pragma once
+
+// #undef VENDOR_ID
+// #undef PRODUCT_ID
+
+// #define VENDOR_ID 0x0d39
+// #define PRODUCT_ID 0x0001
+
+
+
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x78, 0xF7, 0xF8, 0xC5, 0x38, 0x19, 0x03, 0x6B}

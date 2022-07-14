ed901be696dc6522fd0229e0b57b8e8ae8504d0a
adamdelisi <51133980+adamdelisi@users.noreply.github.com>
2022-07-13 21:00:33 -0400

    fix macro

--- keyboards/umamamacro/keymaps/default/keymap.c
+++ keyboards/umamamacro/keymaps/default/keymap.c
@@ -3,7 +3,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	KEYMAP(
-		KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9),
+		KC_MPRV, KC_MPLY, KC_MNXT, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18),
 
 	KEYMAP(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
@@ -52,55 +52,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-	//keyevent_t event = record->event;
+// const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+// 	//keyevent_t event = record->event;
 
-	switch (id) {
+// 	switch (id) {
 
-	}
-	return MACRO_NONE;
-}
+// 	}
+// 	return MACRO_NONE;
+// }
 
-void matrix_init_user(void) {
-}
+// void matrix_init_user(void) {
+// }
 
-void matrix_scan_user(void) {
-}
+// void matrix_scan_user(void) {
+// }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	return true;
-}
+// bool process_record_user(uintF116_t keycode, keyrecord_t *record) {
+// 	return true;
+// }
 
-void led_set_user(uint8_t usb_led) {
+// void led_set_user(uint8_t usb_led) {
 
-	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
+// 	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
 		
-	} else {
+// 	} else {
 		
-	}
+// 	}
 
-	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
+// 	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
 		
-	} else {
+// 	} else {
 		
-	}
+// 	}
 
-	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
+// 	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
 		
-	} else {
+// 	} else {
 		
-	}
+// 	}
 
-	if (usb_led & (1 << USB_LED_COMPOSE)) {
+// 	if (usb_led & (1 << USB_LED_COMPOSE)) {
 		
-	} else {
+// 	} else {
 		
-	}
+// 	}
 
-	if (usb_led & (1 << USB_LED_KANA)) {
+// 	if (usb_led & (1 << USB_LED_KANA)) {
 		
-	} else {
+// 	} else {
 		
-	}
+// 	}
 
-}
+// }
